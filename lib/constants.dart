3c71b54d818525661123287e5a355e7b10026152
windo <herwindo.artono@go-jek.com>
2019-06-25 07:17:34 +0700

    wip result ui

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,14 @@ const kInactiveBgColor = Color(0xFF111328);
 const kBottomContainerHeight = 80.0;
 const kBottomBtnColor = Color(0xFFEB1555);
 
+Map<String, double> kFontSize = {
+  'base': 16.0,
+  'small': 9.88,
+  'h2': 25.888,
+  'h1': 41.887,
+  'xl': 67.773,
+};
+
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
   color: Color(0xFF8D8E98),
@@ -19,4 +27,22 @@ const kButtonTextStyle = TextStyle(
   color: Color(0xccFFFFFF),
   fontSize: 20.0,
   fontWeight: FontWeight.w700,
+);
+
+const kResultHeaderTitle = TextStyle(
+  fontSize: 41.89,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF4DC08B),
+);
+
+const kResultNumberTextStyle = TextStyle(
+  fontSize: 67.72,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultCommentTextStyle = TextStyle(
+  fontSize: 25.89,
 );
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'reusable_card.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -7,8 +9,70 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('HELLO'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: Center(
+              child: Text(
+                'YOUR RESULT',
+                style: kResultHeaderTitle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              color: kActiveBgColor,
+              cardChild: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      'NORMAL',
+                      textAlign: TextAlign.center,
+                      style: kResultTextStyle,
+                    ),
+                    Text(
+                      '50.7',
+                      style: kResultNumberTextStyle,
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      'You have normal body weight. Good Job',
+                      style: kResultCommentTextStyle,
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Expanded(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: Center(
+                  child: Text(
+                    'RE-CALCULATE',
+                    style: kButtonTextStyle,
+                  ),
+                ),
+                color: kBottomBtnColor,
+                width: double.infinity,
+                height: kBottomContainerHeight,
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
